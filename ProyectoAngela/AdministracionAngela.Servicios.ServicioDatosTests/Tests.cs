68a0cc9df9f93552000548167582e943a214e798
Alvarito <Alvarito@ALVARO>
2017-08-25 12:00:08 +0200

    añadiendo xunit

--- ProyectoAngela/AdministracionAngela.Servicios.ServicioDatosTests/Tests.cs
+++ ProyectoAngela/AdministracionAngela.Servicios.ServicioDatosTests/Tests.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AdministracionAngela.Servicios.ServicioDatosTests
 {
-    public class Class1
+    public class Tests
     {
+        public Tests()
+        {
+
+        }
+
+        [Fact]
+        public void TEstDeprueba()
+        {
+            Assert.True(true);
+        }
     }
 }

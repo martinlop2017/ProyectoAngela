4b2029362860b205f195f0b39609c77ebf143812
agarcia12 <agarcia12@DER9025MBW>
2017-09-07 12:24:21 +0200

    anadido AltaArticuloViewModel

---
+++ ProyectoAngela/AdministracionAngela.Utils/Models/Articulo/AltaArticuloViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionAngela.Utils.Models.Articulo
+{
+    public class AltaArticuloViewModel
+    {
+        public string Descripcion { get; set; }
+    }
+}

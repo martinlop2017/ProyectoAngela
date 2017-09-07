5495d0b51ec83c6753c4369c288a198a8b518642
agarcia12 <agarcia12@DER9025MBW>
2017-09-07 15:51:24 +0200

    inyectando ivaprovider y repository

--- ProyectoAngela/AdministracionAngela.Servicios.ServicioFactura/IVAProvider.cs
+++ ProyectoAngela/AdministracionAngela.Servicios.ServicioFactura/IVAProvider.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdministracionAngela.Servicios.ServicioDatos.Repositorios;
 using AdministracionAngela.Utils.Interfaces;
 
 namespace AdministracionAngela.Servicios.ServicioDatos
 {
     public class IVAProvider : IIVAProvider
     {
-        public IVAProvider()
+        private IRepositoryIVA repositoryIVA;
+
+        public IVAProvider(IRepositoryIVA repositoryIva)
         {
-            
+            this.repositoryIVA = repositoryIva;
         }
     }
 }

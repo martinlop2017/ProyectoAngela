4e223785270b076ed024cd31be29b50a5f8b695b
agarcia12 <agarcia12@DER9025MBW>
2017-09-07 15:49:59 +0200

    anadiendo repositoryIVA

---
+++ ProyectoAngela/AdministracionAngela.Servicios.ServicioFactura/Repositorios/IVARepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdministracionAngela.EFRepository;
+
+namespace AdministracionAngela.Servicios.ServicioDatos.Repositorios
+{
+    public class IVARepository : IRepositoryIVA
+    {
+        private IAdministracionAngelaContext dbContext;
+
+        public IVARepository(IAdministracionAngelaContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}

7ce5b93aac898f79618fdd8ec9da215fa2905b46
agarcia12 <agarcia12@DER9025MBW>
2017-09-07 12:38:54 +0200

    inyectando repositoryArticulo

--- ProyectoAngela/AdministracionAngela.Servicios.ServicioFactura/ArticuloProvider.cs
+++ ProyectoAngela/AdministracionAngela.Servicios.ServicioFactura/ArticuloProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdministracionAngela.Servicios.ServicioDatos.Repositorios;
 using AdministracionAngela.Utils.Interfaces;
 using AdministracionAngela.Utils.Mappers;
 using AdministracionAngela.Utils.Models.Articulo;
@@ -11,9 +12,11 @@ namespace AdministracionAngela.Servicios.ServicioDatos
 {
     public class ArticuloProvider : IArticuloProvider
     {
-        public ArticuloProvider()
+        private IRepositorioArticulo repositoryArticulo;
+
+        public ArticuloProvider(IRepositorioArticulo repositoryArticulo)
         {
-            
+            this.repositoryArticulo = repositoryArticulo;
         }
 
         public void SaveArticulo(AltaArticuloViewModel nuevoArticulo)

832e741219fbb6780879fe2ff78c4a5d8d6eedb7
Alvarito <Alvarito@ALVARO>
2017-09-08 19:15:34 +0200

    campos de perfilViewModel

--- ProyectoAngela/AdministracionAngela.Utils/Models/Perfil/PerfilViewModel.cs
+++ ProyectoAngela/AdministracionAngela.Utils/Models/Perfil/PerfilViewModel.cs
@@ -8,5 +8,22 @@ namespace AdministracionAngela.Utils.Models.Perfil
 {
     public class PerfilViewModel
     {
+        public string Nombre { get; set; }
+        public string NIF { get; set; }
+        public string Direccion { get; set; }
+        public int CodigoPostal { get; set; }
+        public string Poblacion { get; set; }
+        public string Provincia { get; set; }
+        public int Telefono1 { get; set; }
+        public int Telefono2 { get; set; }
+        public int Fax { get; set; }
+        public string Email { get; set; }
+        public string Iban { get; set; }
+
+        public PerfilViewModel()
+        {
+
+        }
+
     }
 }

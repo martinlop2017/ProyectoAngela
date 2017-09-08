c307cf2e4119f3ec1c8ec0f2c856954586d3381c
Alvarito <Alvarito@ALVARO>
2017-09-08 19:12:15 +0200

    clase anadida PerfilViewModel

---
+++ ProyectoAngela/AdministracionAngela.Utils/Models/Perfil/PerfilViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionAngela.Utils.Models.Perfil
+{
+    public class PerfilViewModel
+    {
+    }
+}

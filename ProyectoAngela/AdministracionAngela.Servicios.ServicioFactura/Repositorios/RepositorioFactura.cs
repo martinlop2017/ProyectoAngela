dae0ef8abcc5c8fd9a19932a6005e0bf9e89436b
agarcia12 <agarcia12@DER9025MBW>
2017-09-12 15:57:45 +0200

    creando provider y repositorio para factura

---
+++ ProyectoAngela/AdministracionAngela.Servicios.ServicioFactura/Repositorios/RepositorioFactura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionAngela.Servicios.ServicioDatos.Repositorios
+{
+    public class RepositorioFactura : IRepositorioFactura
+    {
+        public RepositorioFactura()
+        {
+            
+        }
+    }
+}

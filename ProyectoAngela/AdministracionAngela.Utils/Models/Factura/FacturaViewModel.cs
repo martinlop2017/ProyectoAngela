09424763086077253ad559f14b141553e2cae886
agarcia12 <agarcia12@DER9025MBW>
2017-09-12 15:51:54 +0200

    anadiendo FacturaViewModel

---
+++ ProyectoAngela/AdministracionAngela.Utils/Models/Factura/FacturaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdministracionAngela.Utils.Models.Factura
+{
+    public class FacturaViewModel
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public string NombreCliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal TotalBase { get; set; }
+        public decimal TotalIVA { get; set; }
+        public decimal TotalRecargoEquivalencia { get; set; }
+    }
+}

7cf118a4bf6acc3c487805aa8cd20d1e3127fd53
agarcia12 <agarcia12@DER9025MBW>
2017-09-13 15:25:34 +0200

    escribiendo test para traer todos los clientes Anadiendo nuget para mocks

---
+++ ProyectoAngela/AdministracionAngela.Servicios.ServicioDatosTests/FacturaProviderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdministracionAngela.Servicios.ServicioDatos;
+using AdministracionAngela.Servicios.ServicioDatos.Repositorios;
+using Xunit;
+using AdministracionAngela.Utils.Interfaces;
+using Moq;
+
+namespace AdministracionAngela.Servicios.ServicioDatosTests
+{
+    public class FacturaProviderTests
+    {
+        private IFacturaProvider facturaProvider;
+        public FacturaProviderTests()
+        {
+        }
+
+        [Fact]
+        public void ShouldReturnClientsInComboFormat()
+        {
+            var mockRepositorioFactura = new Mock<IRepositorioFactura>();
+            this.facturaProvider = new FacturaProvider(mockRepositorioFactura.Object);
+
+            this.facturaProvider.GetFacturaViewModel();
+        }
+    }
+}

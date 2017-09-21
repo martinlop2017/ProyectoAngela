6c92309cefeef4240d92d478896029058c95d124
agarcia12 <agarcia12@DER9025MBW>
2017-09-21 13:22:55 +0200

    anadiendo viewmodels

---
+++ ProyectoAngela/AdministracionAngela.Utils/Models/Factura/FacturaDataGridViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionAngela.Utils.Models.Factura
+{
+    public class FacturaDataGridViewModel
+    {
+        public int CodigoFactura { get; set; }
+        public string Cliente { get; set; }
+    }
+}
---
+++ ProyectoAngela/AdministracionAngela.Utils/Models/Factura/GestionFacturaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionAngela.Utils.Models.Factura
+{
+    public class GestionFacturaViewModel
+    {
+        public BindingList<FacturaDataGridViewModel> Facturas { get; set; }
+
+        public GestionFacturaViewModel()
+        {
+            this.Facturas = new BindingList<FacturaDataGridViewModel>();
+        }
+    }
+}

d5b3ded08dfdbc006e5a97cad2f8485da7c005e5
Alvarito <Alvarito@ALVARO>
2017-12-16 15:53:23 +0100

    Creando sistemaProvider y repositorioSistema

---
+++ ProyectoAngela/AdministracionAngela.Servicios.ServicioFactura/Repositorios/RepositorioSistema.cs
@@ -0,0 +1,19 @@
+﻿using AdministracionAngela.EFRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionAngela.Servicios.ServicioDatos.Repositorios
+{
+    public class RepositorioSistema : IRepositorioSistema
+    {
+        IAdministracionAngelaContext dbContext;
+
+        public RepositorioSistema(IAdministracionAngelaContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
---
+++ ProyectoAngela/AdministracionAngela.Servicios.ServicioFactura/SistemaProvider.cs
@@ -0,0 +1,22 @@
+﻿using AdministracionAngela.Servicios.ServicioDatos.Repositorios;
+using AdministracionAngela.Utils.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionAngela.Servicios.ServicioDatos
+{
+    public class SistemaProvider : ISistemaProvider
+    {
+        private IRepositorioSistema repositorioSistema;
+
+        public SistemaProvider(IRepositorioSistema reposirotiorSistema)
+        {
+            this.repositorioSistema = repositorioSistema;
+        }
+
+
+    }
+}
